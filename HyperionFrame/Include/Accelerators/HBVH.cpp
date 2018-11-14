094e148d4c92f89434802cdf022fde2708ce7b89
李翰博 <xatian23@126.com>
2018-11-14 22:52:43 +0800

    temp

--- HyperionFrame/Include/Accelerators/HBVH.cpp
+++ HyperionFrame/Include/Accelerators/HBVH.cpp
@@ -1,18 +1,92 @@
 #include "HBVH.h"
 
-bool HBVHTree::BuildTreeWithScene(HScene * scene)
+bool HBVHTree::BuildTreesWithScene(HScene * scene)
 {
-	AABB aabb = scene->GetAABB();
-	int dim = aabb.GetMaximumExtent();
+	node = new HBVHTreeNodeAABB();
 
-	vector<HBVHAABBInfo> boundInfo;
-	for (vector<Shape*>::iterator it = scene->shapes.begin(), int count = 0; it != scene->shapes.end(); it++, count++)
+	int count = 0;
+	for (vector<Shape*>::iterator it = scene->shapes.begin(); it != scene->shapes.end(); it++)
 	{
-		HBVHAABBInfo info;
+		HBVHInfoAABB info;
 		info.data = (*it)->GetAABB();
-		info.index = count;
-		boundInfo.push_back(info);
+		info.index = count++;
+		m_boundInfo.push_back(info);
+		node->aabb.Merge(info.data);
+
+		//auto currShapeNode = new HBVHTreeNodeTriangle();
+		//for (int i = 0; i < (*it)->GetFaceCount(); i++)
+		//{
+		//	HBVHInfoTriangle triInfo;
+		//	Triangle tri = (*it)->GetFace(i);
+
+		//	triInfo.index = i;
+		//	for (int j = 0; j < 3; j++)
+		//	{
+		//		triInfo.aabb.Merge(tri.p[j]);
+		//		triInfo.data.p[j] = tri.p[j];
+		//	}
+
+		//	BuildRecursive(currShapeNode);
+		//}
+
+		//shapesNode.push_back(currShapeNode);
 	}
 
+	BuildRecursive(node, m_boundInfo.begin(), m_boundInfo.end());
 	return false;
 }
+
+void HBVHTree::BuildRecursive(HBVHTreeNodeAABB* node, vector<HBVHInfoAABB>::iterator itBegin, vector<HBVHInfoAABB>::iterator itEnd)
+{
+	if (itBegin + 1 == itEnd)
+	{
+		// leaf
+		node->child[0] = node->child[1] = nullptr;
+		node->aabb = (*itBegin).data;
+	}
+	else
+	{
+		int dim = node->aabb.GetMaximumExtent();
+		float midPos = XMVectorGetByIndex(XMLoadFloat3(&node->aabb.GetCenter()), dim);
+		auto split = partition(m_boundInfo.begin(), m_boundInfo.end(), [dim, midPos](HBVHInfoAABB& info)
+		{
+			auto boundPos = XMVectorGetByIndex(XMLoadFloat3(&info.data.GetCenter()), dim);
+			return boundPos < midPos;
+		});
+
+		if (split == itBegin || split == itEnd)
+		{
+			// bad result, so leaf
+			node->child[0] = node->child[1] = nullptr;
+		}
+		else
+		{
+			// interior
+			BuildRecursive(node, itBegin, split);
+			BuildRecursive(node, split, itEnd);
+		}
+	}
+}
+
+//void BuildRecursive(HBVHTreeNodeTriangle* node, vector<HBVHInfoTriangle>::iterator itBegin, vector<HBVHInfoTriangle>::iterator itEnd) 
+//{
+//	int dim = node->aabb.GetMaximumExtent();
+//	float midPos = XMVectorGetByIndex(XMLoadFloat3(&node->aabb.GetCenter()), dim);
+//	auto split = partition(m_boundInfo.begin(), m_boundInfo.end(), [dim, midPos](HBVHInfoAABB& info)
+//	{
+//		auto boundPos = XMVectorGetByIndex(XMLoadFloat3(&info.data.GetCenter()), dim);
+//		return boundPos < midPos;
+//	});
+//
+//	if (split == itBegin || split == itEnd)
+//	{
+//		// bad result, so leaf
+//		node->child[0] = node->child[1] = nullptr;
+//	}
+//	else
+//	{
+//		// interior
+//		BuildRecursive(node, itBegin, split);
+//		BuildRecursive(node, split, itEnd);
+//	}
+//}
--- HyperionFrame/Include/Accelerators/HBVH.h
+++ HyperionFrame/Include/Accelerators/HBVH.h
@@ -1,17 +1,42 @@
 #pragma once
 #include "HScene.h"
 
-struct HBVHAABBInfo
+struct HBVHInfoAABB
 {
 	int index;
 	AABB data;
 };
 
+//struct HBVHInfoTriangle
+//{
+//	int index;
+//	AABB aabb;
+//	Triangle data;
+//};
+
+struct HBVHTreeNodeAABB
+{
+	HBVHTreeNodeAABB* child[2];
+	AABB aabb;
+};
+
+//struct HBVHTreeNodeTriangle
+//{
+//	HBVHTreeNodeTriangle* child[2];
+//	AABB aabb;
+//	Triangle triangle;
+//};
+
 class HBVHTree
 {
 public:
-	bool BuildTreeWithScene(HScene* scene);
-	void BuildRecursive();
+	bool BuildTreesWithScene(HScene* scene);
+	void BuildRecursive(HBVHTreeNodeAABB* node, vector<HBVHInfoAABB>::iterator itBegin, vector<HBVHInfoAABB>::iterator itEnd);
+	//void BuildRecursive(HBVHTreeNodeTriangle* node, vector<HBVHInfoTriangle>::iterator itBegin, vector<HBVHInfoTriangle>::iterator itEnd);
 
 private:
+	vector<HBVHInfoAABB> m_boundInfo;
+	HBVHTreeNodeAABB* node;
+	//vector<vector<HBVHInfoTriangle>> m_shapesBoundInfo;
+	//vector<HBVHTreeNodeTriangle*> shapesNode;
 };
